94981469c63eb65023eda1e1728c5221c5514c0b
ShashwatSolanki <shashwatsolanki786@gmail.com>
2026-02-21 11:55:02 +0530

    User Schema updated for user creation

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,11 +1,10 @@
-from pydantic import BaseModel, EmailStr
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from uuid import UUID
 
 
 class UserCreate(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(min_length=8, max_length=72)
     organization_id: UUID
 
 
