9cf10fabcce37bf146e4eff10f7c99ed224b30a4
ShashwatSolanki <shashwatsolanki786@gmail.com>
2026-02-21 11:40:26 +0530

    Adding a register endpoint

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -6,6 +6,10 @@ from app.models.user import User
 from app.schemas.user import UserLogin
 from app.core.security import verify_password, create_access_token
 
+from app.core.security import hash_password
+from app.schemas.user import UserCreate, UserResponse
+from app.models.organization import Organization
+
 router = APIRouter()
 
 
@@ -42,3 +46,39 @@ def login(user_data: UserLogin, db: Session = Depends(get_db)):
     access_token = create_access_token(token_data)
 
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post("/register", response_model=UserResponse)
+def register(user_data: UserCreate, db: Session = Depends(get_db)):
+
+    # Check if organization exists
+    org = db.query(Organization).filter(
+        Organization.id == user_data.organization_id
+    ).first()
+
+    if not org:
+        raise HTTPException(status_code=400, detail="Organization not found")
+
+    # Check duplicate email inside tenant
+    existing_user = db.query(User).filter(
+        User.email == user_data.email,
+        User.organization_id == user_data.organization_id
+    ).first()
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already exists in this organization")
+
+    # Hash password
+    hashed_password = hash_password(user_data.password)
+
+    new_user = User(
+        email=user_data.email,
+        password_hash=hashed_password,
+        organization_id=user_data.organization_id
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
