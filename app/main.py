51bbfcb48ed68a55fc6683b2fade5d9d70bd42b7
ShashwatSolanki <shashwatsolanki786@gmail.com>
2026-02-21 11:37:34 +0530

    Adding Auth routr in main

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 
 from app.db.base import Base
 from app.db.session import engine
+from app.api.routes import auth
 
 app = FastAPI()
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 
 
 @app.on_event("startup")
