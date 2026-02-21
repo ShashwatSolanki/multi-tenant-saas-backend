ef91e5836e2925f3e6f4a43dc3eb032e0afc6ef5
ShashwatSolanki <shashwatsolanki786@gmail.com>
2026-02-21 12:01:46 +0530

    Creating Dependencies in Security.py

--- app/core/security.py
+++ app/core/security.py
@@ -2,12 +2,49 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
+from fastapi.security import OAuth2PasswordBearer
+
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.db.session import SessionLocal
+from app.models.user import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
 SECRET_KEY = "supersecretkey"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    payload = decode_access_token(token)
+
+    if not payload:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = db.query(User).filter(
+        User.id == payload.get("user_id")
+    ).first()
+
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
