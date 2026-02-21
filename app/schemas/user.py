585ef36c026bd5f244c5e25239fdbababa5c17a9
ShashwatSolanki <shashwatsolanki786@gmail.com>
2026-02-21 11:39:30 +0530

    Updating User Schema and adding UserCreate and UserResponse

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,5 +1,22 @@
 from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr
+from uuid import UUID
 
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    organization_id: UUID
+
+
+class UserResponse(BaseModel):
+    id: UUID
+    email: EmailStr
+    organization_id: UUID
+    is_active: bool
+
+    class Config:
+        from_attributes = True
 
 class UserLogin(BaseModel):
     email: EmailStr
