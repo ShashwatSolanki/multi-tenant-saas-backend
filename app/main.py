b12ae0a287cfe07579ef3426dc6575b6eba00621
ShashwatSolanki <shashwatsolanki786@gmail.com>
2026-02-27 12:46:56 +0530

    Registering routes in main.py

--- app/main.py
+++ app/main.py
@@ -2,16 +2,25 @@ from fastapi import FastAPI
 
 from app.db.base import Base
 from app.db.session import engine
+
 from app.api.routes import auth
+from app.middleware.auth import AuthMiddleware
+from app.middleware.tenant import TenantMiddleware
+from app.middleware.rbac import RBACMiddleware
 
 app = FastAPI()
-app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 
+# Middleware Order Matters
+app.add_middleware(AuthMiddleware)
+app.add_middleware(TenantMiddleware)
+app.add_middleware(RBACMiddleware)
+
+# Routes
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 
 @app.on_event("startup")
 def create_tables():
     Base.metadata.create_all(bind=engine)
-
 
 @app.get("/health")
 def health_check():
